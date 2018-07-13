c7e8ada28eb1c121388bb7bb7bc9ab59a913cf61
Pedro Sotero Almeida <palmeida.terceiro@brasilcap.com.br>
2018-07-13 17:12:16 -0300

    removing class, compact to a single function

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 'use strict';
 
+let getPropertiesS3 = (() => {
+    var _ref = _asyncToGenerator(function* (bucket, keys) {
+        const results = [];
+        for (let i = 0; i < keys.length; i += 1) {
+            const result = yield Promise.promisify(aws.S3_Get)(bucket, keys[i].valor);
+            results[keys[i].chave] = JSON.parse(result.Body);
+        }
+        return results;
+    });
+
+    return function getPropertiesS3(_x, _x2) {
+        return _ref.apply(this, arguments);
+    };
+})();
+
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 const aws = require('brcap-aws');
 const Promise = require('bluebird');
 
-class Properties {
-    constructor(bucket, keys) {
-        this.bucket = bucket;
-        this.keys = keys;
-    }
-
-    getPropertiesS3() {
-        var _this = this;
-
-        return _asyncToGenerator(function* () {
-            const results = [];
-            for (let i = 0; i < _this.keys.length; i += 1) {
-                const result = yield Promise.promisify(aws.S3_Get)(_this.bucket, _this.keys[i].valor);
-                results[_this.keys[i].chave] = JSON.parse(result.Body);
-            }
-            return results;
-        })();
-    }
-}
-
-module.exports = Properties;
+module.exports = {
+    getPropertiesS3
+};
 
--- src.js
+++ src.js
@@ -1,20 +1,15 @@
 const aws = require('brcap-aws');
 const Promise = require('bluebird');
 
-class Properties {
-    constructor(bucket, keys) {
-        this.bucket = bucket;
-        this.keys = keys;
+async function getPropertiesS3(bucket, keys) {
+    const results = [];
+    for (let i = 0; i < keys.length; i += 1) {
+        const result = await Promise.promisify(aws.S3_Get)(bucket, keys[i].valor);
+        results[keys[i].chave] = JSON.parse(result.Body);
     }
-
-    async getPropertiesS3() {
-        const results = [];
-        for (let i = 0; i < this.keys.length; i += 1) {
-            const result = await Promise.promisify(aws.S3_Get)(this.bucket, this.keys[i].valor);
-            results[this.keys[i].chave] = JSON.parse(result.Body);
-        }
-        return results;
-    }
+    return results;
 }
 
-module.exports = Properties;
+module.exports = {
+    getPropertiesS3,
+};
